741b7ca531861062ff9037327aa10057c2b96039
Maya <maya.husic@gmail.com>
2019-07-17 13:54:56 -0400

    handle ajax request and deal with response

--- api/api.py
+++ api/api.py
@@ -2,6 +2,8 @@ import flask
 from flask import request, current_app, make_response
 from flask import render_template
 
+# The flask request object (from flask import request) contains data that the browser sends to this app (the url parameters)
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -11,6 +13,21 @@ def home():
 
 
 # http://antipode/location?longitude=33.7679192&latitude=84.5606917
+def antipode_coords(latitude, longitude):
+  anti_l = 0.0
+  anti_long = 0.0
+  if latitude <= 90 or latitude > 0:
+    anti_l = latitude * -1
+
+  if longitude < 0:
+    anti_long = longitude + 180
+  elif longitude > 0:
+    anti_long = longitude -180
+  else:
+    print('Something is not right.')
+
+  return float(anti_l), float(anti_long)
+
 
 @app.route('/antipode/', methods=['GET', 'OPTIONS'])
 def antipode():
@@ -19,7 +36,8 @@ def antipode():
         return response
     latitude = request.args.get('latitude')
     longitude = request.args.get('longitude')
-    response = flask.jsonify({'latitude':latitude},{'longitude':longitude})
+    anti_lat, anti_long = antipode_coords(float(latitude), float(longitude))
+    response = flask.jsonify({'latitude':anti_lat},{'longitude':anti_long})
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
